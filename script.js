a1936feda36c22e2030cc08abf0ce45b2049f4f7
bel4enok03 <belskaya-tanyush@mail.ru>
2022-10-15 03:07:34 +0400

    *

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-window.addEventListener('scroll',function(){
-    let header = this.document.querySelector('header');
-    header.classList.toggle('sticky',window.scrollY > 0);
+window.addEventListener('scroll', function () {
+	let header = this.document.querySelector('header');
+	header.classList.toggle('sticky', window.scrollY > 0);
 });
 
 let menuToggle = document.querySelector('.toggle');
 let menu = document.querySelector('.menu');
 
-menuToggle.onclick = () =>{
-    menuToggle.classList.toggle('active');
-    menu.classList.toggle('active');
-}
+menuToggle.onclick = () => {
+	menuToggle.classList.toggle('active');
+	menu.classList.toggle('active');
+};
 
-window.onscroll = () =>{
-    menu.classList.remove('active');
-    menuToggle.classList.remove('active');
-}
+window.onscroll = () => {
+	menu.classList.remove('active');
+	menuToggle.classList.remove('active');
+};
