f800bc84ce586fc80e8377acde8cc367d251a3c2
bel4enok03 <belskaya-tanyush@mail.ru>
2022-10-15 01:31:50 +0400

    **

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 window.addEventListener('scroll',function(){
-    let header = this.document.querySelector('header')
+    let header = this.document.querySelector('header');
     header.classList.toggle('sticky',window.scrollY > 0);
 });
+
+let menuToggle = document.querySelector('.toggle');
+menuToggle.onclick = () =>{
+    menuToggle.classList.toggle('active');
+}
+
